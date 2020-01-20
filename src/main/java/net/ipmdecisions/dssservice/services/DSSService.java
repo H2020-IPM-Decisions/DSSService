b0ae74000c4e2e0eb1c94f123f08c1f9833d5597
Tor-Einar Skog <tor-einar.skog@nibio.no>
2020-01-20 09:20:17 +0100

    License change and incorporation into files

--- src/main/java/net/ipmdecisions/dssservice/services/DSSService.java
+++ src/main/java/net/ipmdecisions/dssservice/services/DSSService.java
@@ -1,8 +1,22 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2020 NIBIO <http://www.nibio.no/>. 
+ * 
+ * This file is part of IPMDecisionsDSSService.
+ * IPMDecisionsDSSService is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * IPMDecisionsDSSService is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with IPMDecisionsDSSService.  If not, see <http://www.gnu.org/licenses/>.
+ * 
  */
+
 
 package net.ipmdecisions.dssservice.services;
 
